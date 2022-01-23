b5f36fac6331a5a2a23b9ca302b2bd827fc74ddf
Sharad2001 <57340172+Sharad2001@users.noreply.github.com>
2022-01-23 17:29:34 +0530

    Number of occurrence of character in a string.

---
+++ w3_84.py
@@ -0,0 +1,7 @@
+def no_of_occurence(s):
+    return s.count(occ)
+
+s = input("Enter the string: ")
+occ = input("Enter the character: ")
+
+print(f"No. of occurence of '{occ}' in string is '{no_of_occurence(s)}'")

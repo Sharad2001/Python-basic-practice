2fcea8dce27af87fcffc034f92843014a52df8c0
Sharad2001 <57340172+Sharad2001@users.noreply.github.com>
2022-02-17 13:59:59 +0530

    Check for a string to be palindrome.
    It takes an input from user and check whether a given string is palindrome or not.

--- palindrome.py
+++ palindrome.py
@@ -1,12 +1,19 @@
-s = input("Enter string: ")
+# Method 1:
+s = input("Enter text: ")
 low = 0
-high = len(s) - 1
+high = len(s)-1
 while low < high:
     if s[low] != s[high]:
-        print("No")
+        print("Sorry! Given text is not palindrome.")
         break
     low = low+1
     high = high-1
-
 else:
-    print("Yes")
+    print("Yes the given text is palindrome.")
+
+# Method 2:
+# s = input("Enter the string: ")
+# if s == s[::-1]:
+#     print("Yes the given string is palindrome.")
+# else:
+#     print("Sorry!! Given string is not a palindrome.")

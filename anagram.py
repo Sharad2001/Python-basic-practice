e8c30d40db804cd96e1d564a0aa9fc8f05b26887
Sharad2001 <57340172+Sharad2001@users.noreply.github.com>
2022-02-18 08:55:14 +0530

    Check for anagram.
    Program will asked user to give two different input as string and it will return True if both strings are anagram otherwise it will returned False.

---
+++ anagram.py
@@ -0,0 +1,16 @@
+def areAnagram(s1, s2):
+    if len(s1) != len(s2):
+        return False
+
+    count = [0]*256
+
+    for i in range(len(s1)):
+        count[ord(s1[i])] += 1
+        count[ord(s2[i])] -= 1
+
+    for x in count:
+        if x != 0:
+            return False
+    return True
+
+print(areAnagram(input("Enter the first string: "), input("Enter the second string: ")))

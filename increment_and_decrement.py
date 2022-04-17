564b4961311841df712fe323a8373b13be336320
Sharad2001 <vsharad2001@gmail.com>
2022-04-17 20:18:25 +0530

    Increment and decrement

---
+++ increment_and_decrement.py
@@ -0,0 +1,19 @@
+def do_operation(X, Y):
+
+    X -= 1
+    Y += 1
+    print(X)
+    print(Y)
+
+def main():
+    testcase = int(input())
+
+    while (testcase > 0):
+        input1 = input().split()
+        X = int(input1[0])
+        Y = int(input1[1])
+        do_operation(X, Y)
+        testcase -= 1
+
+if __name__ == '__main__':
+    main()

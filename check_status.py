aca6b6627c79c58304a597ab952b02882f57ef61
Sharad2001 <vsharad2001@gmail.com>
2022-04-22 21:41:24 +0530

    Check the status
    Given two integer variables a and b, and a boolean variable flag. The task is to check the status and return accordingly.
    Return True for the following cases:
    
    Either a or b is non-negative and flag is false.
    Both a and b are negative and flag is true.
    Otherwise, return false.

---
+++ check_status.py
@@ -0,0 +1,23 @@
+def check_status(a, b, flag):
+    if (a > 0 and b > 0):
+        return False
+    if ((a > 0 or b > 0) and flag == False) or ((a < 0 and b < 0) and flag == True):
+        return True
+    else:
+        return False
+
+def main():
+    a = int(input())
+    b = int(input())
+    flag = input()
+    if (flag == "True"):
+        flag = True
+    else:
+        flag = False
+    if (check_status(a, b, flag) is True):
+        print("True")
+    else:
+        print("False")
+
+if __name__ == '__main__':
+    main()

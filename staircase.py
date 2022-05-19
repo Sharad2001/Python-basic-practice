ee64590c521b45f5e4f14cadc8af68f2eb9d903b
Sharad2001 <vsharad2001@gmail.com>
2022-05-19 22:33:01 +0530

    Staircase
    Print staircase on size n

---
+++ staircase.py
@@ -0,0 +1,13 @@
+def staircase(n):
+    for i in range(0, n):
+        for j in range(0, n):
+            if i + j >= n-1:
+                print("#", end="")
+            else:
+                print(" ", end = "")
+        print("\r")
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    staircase(n)
